d56ebc30c71934b46705a34df7e99efc218cd712
Dilusha Gonagala <andrewdexis@gmail.com>
2016-02-01 14:01:42 +0530

    Express Commit

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var port  	 = process.env.PORT || 8000;
 //define the port to use
 app.use(express.static(__dirname + '/public'));
 
+//Main Route Sample
+app.get('/', function (req, res) {
+  res.send('Hello Microservice API !');
+});
 //call listen method
 app.listen(port);
 console.log("App listening on port " + port);
