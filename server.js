d7fc8281e6e611910675988ead15ae68d3a22ac6
Dilusha Gonagala <andrewdexis@gmail.com>
2016-02-01 17:20:59 +0530

    Express Commit

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ var app      = express();
 //port configuration
 var port  	 = process.env.PORT || 8000;
 
-//define the port to use
-app.use(express.static(__dirname + '/public'));
-
 //require API File
 require('./app/api.js')(app);
 
